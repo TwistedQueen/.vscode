8853b23a08f65e4f47f5640640c3cb63f1ad8dcc
TwistedQueen <88774979+TwistedQueen@users.noreply.github.com>
2021-08-11 19:41:35 +0200

    .vscode

---
+++ js tutorial/tutorial.js
@@ -0,0 +1,6 @@
+var name ="learjavascript";
+console.log(name);
+
+const age = 23; // unable to change the value if you use constant(const) thats why you use variable(var) if you are going to chenge the value next//
+age = 21;
+console.log(age);
