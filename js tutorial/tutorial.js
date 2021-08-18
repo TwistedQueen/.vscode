1932a1ee0066264fbd0101747b2d409c260d5375
TwistedQueen <88774979+TwistedQueen@users.noreply.github.com>
2021-08-18 09:33:20 +0200

    Update tutorial.js

--- js tutorial/tutorial.js
+++ js tutorial/tutorial.js
@@ -1,6 +1,2 @@
-var name ="learjavascript";
-console.log(name);
-
-const age = 23; // unable to change the value if you use constant(const) thats why you use variable(var) if you are going to chenge the value next//
-
-console.log(age);
+let items = ['color', 'paintbrush','plate'];
+console.log(items);
